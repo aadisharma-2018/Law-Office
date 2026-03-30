9ac7004fd5a20d67fe809919e8cf48c45c0d4688
Sharad Sharma <sharad05@Sharads-MacBook-Air.local>
2026-03-30 16:53:12 -0700

    Stop commiting .DF_Store and .next

--- apps/firm-site/.next/static/chunks/apps_firm-site_src_app_layout_tsx_9187dcda._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__d171adc5._.css",
-  "static/chunks/apps_firm-site_192b89f9._.js"
-],
-    source: "dynamic"
-});
--- apps/firm-site/.next/static/chunks/apps_firm-site_src_app_page_tsx_15d234bf._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/46b8e_next_dist_7fac0132._.js"
-],
-    source: "dynamic"
-});
